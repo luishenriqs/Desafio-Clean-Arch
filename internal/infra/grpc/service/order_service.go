3f8c5f74c6e15f38af6ef19a43bd9f439b522244
Luís Henrique Pereira <lh.p@hotmail.com>
2026-04-03 16:14:38 -0300

    feat: expose list orders via grpc

--- internal/infra/grpc/service/order_service.go
+++ internal/infra/grpc/service/order_service.go
@@ -24,14 +24,40 @@ func (s *OrderService) CreateOrder(ctx context.Context, in *pb.CreateOrderReques
 		Price: float64(in.Price),
 		Tax:   float64(in.Tax),
 	}
+
 	output, err := s.CreateOrderUseCase.Execute(dto)
 	if err != nil {
 		return nil, err
 	}
+
 	return &pb.CreateOrderResponse{
 		Id:         output.ID,
 		Price:      float32(output.Price),
 		Tax:        float32(output.Tax),
 		FinalPrice: float32(output.FinalPrice),
+	}, nil
+}
+
+func (s *OrderService) ListOrders(ctx context.Context, in *pb.Blank) (*pb.ListOrdersResponse, error) {
+	listOrders := usecase.NewListOrdersUseCase(s.CreateOrderUseCase.OrderRepository)
+
+	output, err := listOrders.Execute()
+	if err != nil {
+		return nil, err
+	}
+
+	orders := make([]*pb.Order, 0, len(output))
+
+	for _, order := range output {
+		orders = append(orders, &pb.Order{
+			Id:         order.ID,
+			Price:      float32(order.Price),
+			Tax:        float32(order.Tax),
+			FinalPrice: float32(order.FinalPrice),
+		})
+	}
+
+	return &pb.ListOrdersResponse{
+		Orders: orders,
 	}, nil
 }
